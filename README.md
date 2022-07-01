26383639e6a1684d4a503e11f411f8e0a9213ca2
Akilesh-02 <akileshshekar21@gmail.com>
2022-07-01 20:04:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-mci3gf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-mci3gf)
---
+++ src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { ViewModule } from './view/view.module';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule, ViewModule],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
